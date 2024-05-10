948695bbcb16171133779be9beec3160d1546994
Libe Arana <libe.arana@alumni.mondragon.edu>
2024-05-10 12:13:28 +0200

    primer commit

---
+++ datos/ejercicio otra vez.R
@@ -0,0 +1,24 @@
+setwd("C:/ASIGNATURAS/Data Science/ejercicio otra vez/ejercicio-otra-vez/Datos")
+library(readxl)
+library(lubridate)
+library(dplyr)
+datos <- read_xlsx("online_retail_II.xlsx")
+str(datos)
+
+datos$InvoiceDate <- ymd_hms(datos$InvoiceDate)
+datos$Invoice <- as.factor(datos$Invoice)
+
+summary(datos)
+head(datos)
+str(datos)
+class(datos)
+length(datos)
+dim(datos)
+
+pedidos_diferentes<-length(unique(datos$Invoice))
+
+precio_medio<- datos %>% group_by(Invoice) %>% summarise(media=mean(n()))
+
+
+
+
